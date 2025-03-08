99bef3dbfa6629fddec7115c9d56e9481a84a437
Charllcrot <samuelrmillon@gmail.com>
2025-03-08 16:05:32 +0100

    Update StateMachine422.gd

--- StateMachine422.gd
+++ StateMachine422.gd
@@ -1,1 +1,18 @@
 extends Node
+class_name StateMachine
+
+@onready var Insert_here_name_node: Node = self.owner
+
+enum STATE {
+	THISISASTATE,
+	THISISANOTHERSTATE
+}
+
+
+var current_state: STATE = STATE
+func _process(delta):
+	match  current_state:
+		STATE.THISISASTATE:
+			pass
+		STATE.THISISANOTHERSTATE:
+			pass
